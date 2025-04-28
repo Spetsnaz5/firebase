5685b000899362f7ba3f68e7dce39491f8de43b8
liao yi an <4a0g0134@gmail.com>
2025-04-28 10:56:37 +0800

    增加取消訂閱功能

---
+++ unSubscribeFromAllTopics.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Exception\MessagingException;
+
+try {
+
+    $token = $_REQUEST['token'] ?? null;
+
+    if (is_null($token))
+        throw new Exception('無定義 token');
+
+    $factory = (new Factory)->withServiceAccount('my-firebase-adminsdk.json');
+
+    $messaging = $factory->createMessaging();
+
+    $registrationTokens = [
+        $token
+    ];
+
+    # https://github.com/kreait/firebase-php/blob/7.x/docs/cloud-messaging.rst
+    // 取消訂閱所有主題
+    $result = $messaging->unsubscribeFromAllTopics($registrationTokens);
+
+    dd($result);
+
+} catch (MessagingException $e) {
+    dd($e);
+} catch (Exception $e) {
+    dd($e);
+}
