5be93e16eb5f8050f6414e605e47dcda47dc465b
liao yi an <4a0g0134@gmail.com>
2025-04-28 11:54:46 +0800

    增加檢查firebase設定檔

--- subscribeToTopic.php
+++ subscribeToTopic.php
@@ -19,7 +19,11 @@ try {
 
     $topic = $contents['topic'] ?? 'matchday';
 
-    $factory = (new Factory)->withServiceAccount('my-firebase-adminsdk.json');
+    $file = 'my-firebase-adminsdk.json';
+    if (! file_exists($file))
+        throw new Exception('無 Firebase 設定檔');
+
+    $factory = (new Factory)->withServiceAccount($file);
 
     $messaging = $factory->createMessaging();
 
